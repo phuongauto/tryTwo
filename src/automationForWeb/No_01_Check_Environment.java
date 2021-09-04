e8e9b1a3ece800c2ad25f50efabd11b877d3cc41
May cua Long <may@long.vn>
2021-09-04 16:20:20 +0700

    Init tryTwo and first commit, push

---
+++ src/automationForWeb/No_01_Check_Environment.java
@@ -0,0 +1,59 @@
+package automationForWeb;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+public class No_01_Check_Environment {
+
+	WebDriver driver;
+
+	@BeforeClass
+	public void beforeClass() {
+//		System.setProperty("webdriver.gecko.driver", ".\\winLib\\geckodriver.exe");
+//		driver = new FirefoxDriver();
+		System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/winLib/geckodriver.exe");
+		driver = new FirefoxDriver();
+		
+//		System.setProperty("webdriver.chrome.driver", ".\\winLib\\chromedriver.exe");
+//		driver = new ChromeDriver();
+//		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/winLib/chromedriver.exe");
+//		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("http://demo.guru99.com/v4/");
+	}
+
+	@Test
+	public void TC_01_ValidateCurrentUrl() {
+		// Login Page Url matching
+		String loginPageUrl = driver.getCurrentUrl();
+		Assert.assertEquals(loginPageUrl, "http://demo.guru99.com/v4/");
+	}
+
+	@Test
+	public void TC_02_ValidatePageTitle() {
+		// Login Page title
+		String loginPageTitle = driver.getTitle();
+		Assert.assertEquals(loginPageTitle, "Guru99 Bank Home Page");
+	}
+
+	@Test
+	public void TC_03_LoginFormDisplayed() {
+		// Login form displayed
+		Assert.assertTrue(driver.findElement(By.xpath("//form[@name='frmLogin']")).isDisplayed());
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+}
